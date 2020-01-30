a5e2ae99223e073c823cdc08f6c120f5f3c1e117
Jorge Sanchez <jorge-l_08@outlook.com>
2020-01-29 20:37:16 -0500

    dev jsanchez: programa para contar clicks

---
+++ JavaApplication8/src/ejercicio1_ejemplo/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1_ejemplo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Ventana v1 = new Ventana();
+        v1.setVisible(true);
+    }
+    
+}
